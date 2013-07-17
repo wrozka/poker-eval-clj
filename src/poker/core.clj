82b595229f8109744e1cfd34b3a224e95e55d33a
Pawel Pierzchala <pawelpierzchala@gmail.com>
2013-07-17 12:23:13 +0200

    Inline flush

--- src/poker/core.clj
+++ src/poker/core.clj
@@ -5,9 +5,7 @@
    :rank (.indexOf (seq "23456789TJQKA") r)})
 
 (defn eval-hand [hand]
-  (letfn [(make-card [[s r]]
-            {:suit ({\D :diamond \H :heart \C :club \S :spade} s) 
-             :rank (.indexOf (seq "23456789TJQKA") r)})
+  (letfn [(make-card [[s r]] [s (.indexOf (seq "23456789TJQKA") r)])
           (low-to-high? [ranks]
             (let [high-card (last (sort ranks))
                   low-card (first (sort ranks))]
@@ -16,19 +14,17 @@
             (map #(mod (+ % 1) 13) ranks)) 
           (straight? [ranks]
             (and (= (count (distinct ranks)) 5)
-              (or (low-to-high? ranks) (low-to-high? (normalize-ace ranks)))))
-          (flush? [suits]
-            (= (count (distinct suits)) 1))]
+              (or (low-to-high? ranks) (low-to-high? (normalize-ace ranks)))))]
     (let [hand (map make-card hand)
-          ranks (map :rank hand)
-          suits (map :suit hand)
+          ranks (map last hand)
+          flush (= (count (distinct (map first hand))) 1)
           rank-counts (vals (frequencies ranks))
           same-rank-max (apply max rank-counts)
           pairs (count (filter #(= % 2) rank-counts))]
       (cond
         (= same-rank-max 4) :four-of-a-kind
-        (and (flush? suits) (straight? ranks)) :straight-flush
-        (flush? suits) :flush
+        (and flush (straight? ranks)) :straight-flush
+        flush :flush
         (straight? ranks) :straight
         (and (= same-rank-max 3) (= pairs 1)) :full-house
         (= same-rank-max 3) :three-of-a-kind
